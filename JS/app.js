c9c7e803490a3875374961de74f7950643a1fd6e
aishwaryas2803 <aishwaryas.2803@gmail.com>
2025-08-11 20:25:31 +0530

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+let a=10;
+let b=20;
+let c=30;
+let d=40;
+let sum=(a+b)
+console.log (sum);
+console.log ('The sum of a, b is :' + sum);
+
+//All variable are case sensitive//
+let name="aishwarya";
+console.log(name);//error
+
+//what is camel case//
+let age=40;
+let myEmployeename=john;
