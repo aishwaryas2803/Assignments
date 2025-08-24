f1c920a1cf90ecf88112ca4e166dd5e58ffe4a73
aishwaryas2803 <aishwaryas.2803@gmail.com>
2025-08-24 17:19:37 +0530

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,67 @@
-let a=10;
-let b=20;
-let c=30;
-let d=40;
-let sum=(a+b)
+/* Var declaration*/
+let a=20;
+let b=8;
+let c = 2;
+let sum=(a+b+c)
 console.log (sum);
-console.log ('The sum of a, b is :' + sum);
+console.log ('The sum of a, b, c is :' + sum);
 
-//All variable are case sensitive//
-let name="aishwarya";
-console.log(name);//error
+// Data types//
+// String//
+let fruit = 'Jackfruit';
+console.log(fruit)
 
-//what is camel case//
-let age=40;
-let myEmployeename=john;
+let country = "India";
+console.log(country);
+
+let result = `Pass`;
+console.log(result);
+
+// Number//
+let num1 = 8;
+let num2 = 3.13;
+
+console.log(num1);
+console.log(num2); 
+
+let integer_number1= -3;
+let integer_number2= -2;
+console.log(integer_number1);
+console.log(integer_number2);
+
+let employeeAge = 55;
+let empID = 12345;
+console.log(`value : ${empID} type:${ typeof empID}`);
+console.log(`value :${employeeAge} type :${ typeof employeeAge}`);
+
+//Boolean//
+let isManager = true ;
+let isEmployee = false;
+console.log(`value :${isManager} type :${ typeof isManager}`);
+console.log(`value :${isEmployee} type :${ typeof isEmployee}`);
+
+//undefined
+
+let name = Aishwarya;//variable declaration
+console.log(`value :${num} type :${ typeof num} `);
+console.log(`value :${name} type :${ typeof name} `);
+name ='Aishwarya';//assignment of a variable (doubt)
+console.log(name);
+console.log(num);
+
+
+//null
+let test=null;
+console.log(`value :${test} type :${ typeof test} `); 
+
+//Reassignment is allowed
+let abc;
+abc=10;
+abc='test';
+console.log(`value :${abc} type :${ typeof abc}`);
+
+//Non Primitive//
+let person = {
+            firstName: "Aishwarya",
+            lastName: "Iyer"
+        };
---
+++ JS/app1.js
@@ -0,0 +1,133 @@
+//Assignment operator : =
+
+let test= 10;
+console.log(test);
+
+// assignment operator
+let a = 2;
+console.log("Assignment: a = 2, a =", a);
+
+// addition assignment operator
+a += 3;  // a = a + 3
+console.log("Addition Assignment: a += 3, a =", a);
+
+// subtraction assignment operator
+a -= 4;  // a = a - 4
+console.log("Subtraction Assignment: a -= 4, a =", a);
+
+// multiplication assignment operator
+a *= 5;  // a = a * 5
+console.log("Multiplication Assignment: a *= 5, a =", a);
+
+// division assignment operator
+a /= 6;  // a = a / 6
+console.log("Division Assignment: a /= 6, a =", a);
+
+// remainder assignment operator
+a %= 7;  // a = a % 7
+console.log("Remainder Assignment: a %= 7, a =", a);
+
+// exponentiation assignment operator
+a **= 8;  // a = a**8
+console.log("Exponentiation Assignment: a **= 8, a =", a);
+
+//Arithmetic operator : + - / * %
+let x = 5;
+
+// addition operator
+console.log("Addition Arithmetic: x + 3 = ", x + 3);
+
+// subtraction operator
+console.log("Subtraction Arithmetic: x - 3 =", x - 3);
+
+// multiplication operator
+console.log("Multiplication Arithmetic: x * 3 =", x * 3);
+
+// division operator
+console.log("Division Arithmetic: x / 3 =", x / 3);
+
+// remainder operator
+console.log("Remainder Arithmetic: x % 3 =", x % 3);
+
+let i = 2;
+
+// increment operator
+console.log("Increment Arithmetic: ++i =", ++i);
+
+// decrement operator
+console.log("Decrement Arithmetic: --i =", --i);
+
+// exponentiation operator
+console.log("Exponentiation: i ** 3 =", i ** 3);
+
+//unary operator : ++,--
+let b =10;
+b = b+1;
+b += b+1;
+b ++;
+console.log(b);
+
+//short hand Math operator : += , -=, /=, *=
+let c=11;
+let d=22;
+let add=10;
+add=add+(c+d);
+add +=(c+d);
+console.log(add);
+
+//conditional operator : <,>,>=,<=,=,!==
+
+let myAge = 25;
+let votingAge = 18;
+
+if (myAge >= votingAge) {
+  console.log("Old enough to vote!");
+} else {
+  console.log("Not old enough to vote.");
+}
+
+if (myAge <= votingAge) {
+  console.log("Old enough to vote!");
+} else {
+  console.log("Not old enough to vote.");
+}
+
+//logical operator : && , ||, ^
+let n = 1;
+
+// && logic both expressions are true
+console.log((n < 4) && (4 >= n));
+
+// only one expression is true
+console.log((n <= 4) && (2 == 4));  
+
+// both expressions are false
+console.log((n > 4) && (n == 4));
+
+// || logic both expressions are true
+console.log((n < 4) || (4 >= n));
+
+// only one expression is true
+console.log((n <= 4) || (1 == 4));  
+
+// both expressions are false
+console.log((n > 4) || (n == 4));
+
+// ! logic both expressions are true
+// NOT on true
+console.log(!true); 
+
+// NOT on false
+console.log(!false); 
+
+// comparison example
+console.log(!(4 <= 3)); 
+
+//Ternary operator : ?:
+let marks = prompt('Enter your marks :');
+
+// check the condition
+let result = (marks >= 40) ? 'pass' : 'fail';
+
+console.log(`You ${result} the exam.`);
+
